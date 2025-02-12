ea949b14ec644c0d43cba93e8ba11fe86b225af0
Antonio Espinoza <a0e0bhj@walmart.com>
2025-02-12 18:20:19 -0300

    Refactor card reassignment to use newCard function

--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import "fmt"
 func main() {
 
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"   // Go will infer the type of the variable
-	card = "Five of Diamonds" // Reassign the value of the variable
+	card := "Ace of Spades" // Go will infer the type of the variable
+	// card = "Five of Diamonds" // Reassign the value of the variable
+	card = newCard() // Call a function that returns a string
 
 	fmt.Println(card)
+}
 
+func newCard() string {
+	return "Five of Diamonds"
 }
