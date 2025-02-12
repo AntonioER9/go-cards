289c89a37ff3d6d0cbdb4165a7793a1cc8bf4a68
Antonio Espinoza <a0e0bhj@walmart.com>
2025-02-12 18:23:28 -0300

    Update main function to demonstrate card slice and iteration

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import "fmt"
 func main() {
 
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades" // Go will infer the type of the variable
+	// card := "Ace of Spades" // Go will infer the type of the variable
 	// card = "Five of Diamonds" // Reassign the value of the variable
-	card = newCard() // Call a function that returns a string
+	// card = newCard() // Call a function that returns a string
 
-	fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
