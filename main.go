be42def567ac4aa6598fb27c11f8c7ac878c2f49
Antonio Espinoza <a0e0bhj@walmart.com>
2025-02-12 18:12:31 -0300

    Add variable reassignment example in main function

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"   // Go will infer the type of the variable
+	card = "Five of Diamonds" // Reassign the value of the variable
+
+	fmt.Println(card)
 
 }
