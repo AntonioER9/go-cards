c5200a5e9951d965d8254eb52d59d14530f0abd4
Antonio Espinoza <a0e0bhj@walmart.com>
2025-02-13 18:07:13 -0300

    Add newDeckFromFile function to load a deck from a file

--- deck.go
+++ deck.go
@@ -42,3 +42,11 @@ func (d deck) toString() string {
 func (d deck) saveToFile(filename string) error {
 	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil { // If there is an error
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
